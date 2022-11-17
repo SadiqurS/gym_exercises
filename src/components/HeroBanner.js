7a8ccc09d7dc0f72455af754c7e7eb1430e32053
Sadiqur Sakib <sadiqursakib42@gmail.com>
2022-11-16 19:01:19 -0500

    updated banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import HeroBannerImage from '../assets/images/banner.png';
-import sadiqurgym from '../assets/images/sadiqurgym.png';
+
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Sadiqur's Fitness Committee</Typography>
@@ -19,7 +19,7 @@ const HeroBanner = () => (
     <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
       Exercise
     </Typography>
-    <img src={sadiqurgym} alt="hero-banner" className="hero-banner-img" />
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
   </Box>
 );
 
